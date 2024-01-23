c5b6ff8af3100b381d2823a3b17b2cd0046e0bb8
sunny <sunny@gmail.com>
2024-01-23 18:42:59 +0900

    ready till 1:26:07

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,22 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+   contactMessage = document.getElementById('contact-message')
+
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_cle7z4v','template_3r5l7ap','#contact-form','-K8sDq-H03DFffifw')
+    .then(() =>  {
+        //show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
